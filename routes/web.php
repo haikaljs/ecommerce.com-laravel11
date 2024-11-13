63a66151a1e54fa81111339146e23380caaeaefe
haikaljs <haikaljavascript@gmail.com>
2024-11-13 22:57:17 +0800

    Template mastering

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('admin', function () {
+    return view('admin.auth.login');
+});
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
